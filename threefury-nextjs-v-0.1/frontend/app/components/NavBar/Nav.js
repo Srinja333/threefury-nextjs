14b0483d503d2209dd2ca9b88419f40069152287
Tanmoy Khanta <madesuccess24@gmail.com>
2023-08-14 18:06:42 +0530

    newAdd

--- threefury-nextjs-v-0.1/frontend/app/components/NavBar/Nav.js
+++ threefury-nextjs-v-0.1/frontend/app/components/NavBar/Nav.js
@@ -3,9 +3,11 @@ import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import logo from '../../assets/logoF.png';
 import 'font-awesome/css/font-awesome.min.css';
-import '../../styles/MyHome.css'
+import '../../styles/MyHome.css';
 // import Home from './pages/Home';
 // import AboutUs from './pages/AboutUs/AboutUs';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Nav = () => {
   const [visible,setVisible]=useState(false)
@@ -19,7 +21,12 @@ const Nav = () => {
         <nav className="navbar navbar-expand-lg fixed-top style-sec">
   <div className="container-fluid">
     <a className="me-5" href="#">
-        <img src={logo} className='logo-img' alt="" />
+    <Image
+      src={logo}
+      width={180}
+      height={50}
+      alt="Picture of the author"
+ />
     </a>
     <button onClick={handleVisibility} className="navbar-toggler ms-5" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -42,7 +49,7 @@ const Nav = () => {
       <a className="nav-link" onClick={() => {window.location.replace("/#modal-contact-form-data"); setVisible(false)}} href="#">Contact Us</a>
     </li>
     <li>
-    <button type="button" onClick={() => {window.location.replace("/#modal-contact-form-data"); setVisible(false)}} className="btn ms-md-3 btn-hover-1-copy color-1" href="12"><span>Get a Quote</span></button></li>  
+    <button type="button" onClick={() => {window.location.replace("/#modal-contact-form-data"); setVisible(false)}} className="ms-md-3 btn-hover-1-copy color-1" href="12"><span>Get a Quote</span></button></li>  
 </ul>
       
     </div>
